215a7b93328bf38e28661f8513278809297ed0c9
tiagonardini <Tiagonardini15@gmail.com>
2024-04-19 23:10:46 -0300

    PRACTICA 3 y 4 correcciones

--- src/main/java/oop2/tp3/ejercicio4/Main.java
+++ src/main/java/oop2/tp3/ejercicio4/Main.java
@@ -16,15 +16,12 @@ public class Main {
 
         var repo = new PersonaRepository(jdbi);
         var personas = repo.buscarPorNombre("Vla");
-        if (personas != null) {
-            for (Persona persona : personas) {
-                System.out.println(persona.nombre() + " " + persona.apellido());
-            }
+        for (Persona persona : personas) {
+            System.out.println(persona.nombre() + " " + persona.apellido());
         }
 
         var persona = repo.buscarId(1L);
-        if (persona != null) {
-            System.out.println(persona.nombre() + " " + persona.apellido());
-        }
+        System.out.println(persona.nombre() + " " + persona.apellido());
+
     }
 }
